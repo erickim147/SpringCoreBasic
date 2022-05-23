df109b304812dd5633d49e982d29c82909059d65
EricKim92 <gudals147@naver.com>
2022-05-23 13:39:56 +0900

    스프링 핵심 원리 이해1 예제 만들기 - AppConfig 리팩토링 및 할인정책 코드 변경

--- src/main/java/hello/SpringCoreBasic/AppConfig.java
+++ src/main/java/hello/SpringCoreBasic/AppConfig.java
@@ -2,20 +2,49 @@ package hello.SpringCoreBasic;
 
 import hello.SpringCoreBasic.Order.OrderService;
 import hello.SpringCoreBasic.Order.OrderServiceImpl;
+import hello.SpringCoreBasic.discount.DiscountPolicy;
 import hello.SpringCoreBasic.discount.FixDiscountPolicy;
+import hello.SpringCoreBasic.discount.RateDiscountPolicy;
+import hello.SpringCoreBasic.member.MemberRepository;
 import hello.SpringCoreBasic.member.MemberService;
 import hello.SpringCoreBasic.member.MemberServiceImpl;
 import hello.SpringCoreBasic.member.MemoryMemberRepository;
 
 public class AppConfig {
 
-    public MemberService memberService(){
+    // 기존 AppConfig 를 리팩토링 하자
+    // memberService와 orderService에는 new MemoryMemberRepository가 중복된다.
+    // 중복된 부분을 제거하고 MemoryMemberRepository를 DB로 변경 하더라도 한 부분만 변경되도록 리팩토링 한다.
+    // 또한, 역할에 따른 구현이 잘 보이지 않기 때문에 한눈에 잘 들어올 수 있도록 리팩토링 하는 것이 중요한다.
 
-        return new MemberServiceImpl(new MemoryMemberRepository());
+// 기존 로직
+//    public MemberService memberService(){
+//
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
+//
+//    public OrderService orderService() {
+//        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+//    }
+
+    // 변경 로직
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    // FixDiscountPolicy -> RateDiscountPolicy로 변경해보자.
+    // 할인 정책을 변경 하더라도 AppConfig의 등장으로 사용 영역의 코드 변경 없이 적용이 가능하다.
+    public DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy(); // 정액 할인 정책
+        return new RateDiscountPolicy(); // 정률 할인 정책
     }
 
 }
